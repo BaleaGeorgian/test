65261c5a086f3a4074e29d2706028375c3805fc2
Balea Georgian <tiberius.balea93@gmail.com>
2018-05-19 11:37:24 +0300

    git config --global user.name "Balea Georgian" Primul commit

---
+++ src/Curs4p4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Curs4p4 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+
+
+        for (int i = 0; i < n; ++i) {
+            x[i] = sc.nextInt();
+        }
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < n; i++)
+            if (x[i] != 0 && x[i] % 2 == 1) {
+                a = a + 1;
+                b = b + x[i];
+
+
+        }
+
+
+
+
+    }
+}
